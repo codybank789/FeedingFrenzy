65503a51610ee5a1ce91937a209b6f0204ff8164
Yuu <trongdung.klnd@gmail.com>
2016-07-14 14:26:51 +0700

    . update res

--- src/stupid/Loader/FishLoader.java
+++ src/stupid/Loader/FishLoader.java
@@ -11,39 +11,188 @@ import java.util.Vector;
  */
 public class FishLoader {
 
+    private Vector<GameImage>[][][] listImage;
     public static final int SIZEOFFISH = 3;
     public static final int TYPEOFFISH = 11;
     public static final int TYPEOFANIMATION = 3;
-    private static final int NUMBERIMAGE = 7;
-
-    private static Vector<GameImage>[][][] listImage;
-
-    static {
-        listImage = new Vector[SIZEOFFISH][TYPEOFFISH][TYPEOFANIMATION];
-        int imageCount = 0;
-        int i = 1;
-            for (int j = 0;j<TYPEOFFISH;j++)
-                for(int k =0;k<TYPEOFANIMATION;k++)
-                {
-                    listImage[i][j][k] = new Vector<>();
-
-                    for (int l = 0;l<NUMBERIMAGE;l++) {
-                        imageCount++;
-
-                        String value = String.valueOf(imageCount);
-                        int length = value.length();
-                        for(int t = 0; t < 3 - length; t++) {
-                            value = "0" + value;
-                        }
-
-                        listImage[i][j][k].add(new GameImage("res/Fish/"+value+".png"));
-                    }
-
-                }
-    }
-
-    public static Vector getImages(int size, int typeFish, int typeAnimation)
+    private GameImage gameImage ;
+    private int numberofimg =1;
+    public FishLoader()
     {
-     return listImage[size][typeFish][typeAnimation];
+        listImage = new Vector[SIZEOFFISH][TYPEOFFISH][TYPEOFANIMATION];
+    }
+    public void loadAll()
+    {
+        int i=1;
+        for (int j = 0;j<TYPEOFFISH;j++) {
+            numberofimg=0;
+            for (int k = 0; k < TYPEOFANIMATION; k++) {
+                for (int l = 0; l < checkType(j, k); l++) {
+                    numberofimg++;
+                    String value = String.valueOf(numberofimg);
+                    for (int m = 0; m < 3 - value.length(); m++)
+                        value = "0" + value;
+                    gameImage = new GameImage("res/Fish" + String.valueOf(j) + "/Fish/" + value + ".png");
+                    listImage[i][j][k].add(gameImage);
+                }
+            }
+        }
+    }
+    private Vector getImages(int size, int typeFish, int typeAnimation)
+    {
+        return listImage[size][typeFish][typeAnimation];
+    }
+    private int checkType(int j,int k)
+    {
+        int check =0;
+        switch (j)
+        {
+            case 1:
+                switch (k){
+                    case 1:
+                        check=14;
+                        break;
+                    case 2:
+                        check=7;
+                        break;
+                    case 3:
+                        check =5;
+                        break;
+                }
+                break;
+            case 2:
+                switch (k){
+                    case 1:
+                        check =7;
+                        break;
+                    case 2:
+                        check =6;
+                        break;
+                    case 3:
+                        check =5;
+                        break;
+                }
+                break;
+            case 3:
+                switch (k){
+                    case 1:
+                        check=12;
+                        break;
+                    case 2:
+                        check =5;
+                        break;
+                    case 3:
+                        check =4;
+                        break;
+                }
+                break;
+            case 4:
+                switch (k){
+                    case 1:
+                        check =15;
+                        break;
+                    case 2:
+                        check =4;
+                        break;
+                    case 3:
+                        check =3;
+                        break;
+                }
+                break;
+            case 5:
+                switch (k){
+                    case 1:
+                        check = 15;
+                        break;
+                    case 2:
+                        check = 5;
+                        break;
+                    case 3:
+                        check = 6;
+                        break;
+                }
+                break;
+            case 6:
+                switch (k){
+                    case 1:
+                        check =14;
+                        break;
+                    case 2:
+                        check =5;
+                        break;
+                    case 3:
+                        check =6;
+                        break;
+                }
+                break;
+            case 7:
+                switch (k){
+                    case 1:
+                        check =14;
+                        break;
+                    case 2:
+                        check = 5;
+                        break;
+                    case 3:
+                        check =6;
+                        break;
+                }
+                break;
+            case 8:
+                switch (k){
+                    case 1:
+                        check =15;
+                        break;
+                    case 2:
+                        check = 4;
+                        break;
+                    case 3:
+                        check =7;
+                        break;
+                }
+                break;
+            case 9:
+                switch (k){
+                    case 1:
+                        check = 2;
+                        break;
+                    case 2:
+                        check =2;
+                        break;
+                    case 3:
+                        check =1;
+                        break;
+                }
+                break;
+            case 10:
+                switch (k){
+                    case 1:
+                        check = 20;
+                        break;
+                    case 2:
+                        check =0;
+                        break;
+                    case 3:
+                        check =0;
+                        break;
+                }
+                break;
+            case 11:
+                switch (k){
+                    case 1:
+                        check =15;
+                        break;
+                    case 2:
+                        check =0;
+                        break;
+                    case 3:
+                        check =0;
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        return check;
     }
 }
